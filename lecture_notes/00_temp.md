ef0e16eb8ce384904ca5319a768ad9ae0a7548b6
anthoniraj <anthoniraja@gmail.com>
2022-07-21 11:59:15 +0530

    Added reference links

---
+++ lecture_notes/00_temp.md
@@ -0,0 +1,6 @@
+# General Instructions
+Creating Java Project Reference Links
+1. VTOP LAB Course Page
+2. GitHub Repository: https://github.com/anthoniraj/java_fall_2022_23
+    (Under lecture_notes directory)
+3. Blog: https://anthoniraja.blogspot.com
--- src/edu/vit/corejava/basics/App.java
+++ src/edu/vit/corejava/basics/App.java
@@ -2,11 +2,12 @@ package edu.vit.corejava.basics;
 
 /**
  * Description: Demo Java Program
+ * 
  * @author: Anthoniraj Amalanathan
  * @since: 19-Jul-2022
  */
 public class App {
     public static void main(String[] args) {
         System.out.println("Demo Java Program!");
-    }    
+    }
 }
