528bf2e726a85c9ba362dc8c038eb855a5fc90e8
anthoniraj <anthoniraja@gmail.com>
2022-07-25 08:43:54 +0530

    modified app.java

--- src/edu/vit/corejava/basics/App.java
+++ src/edu/vit/corejava/basics/App.java
@@ -1,13 +1,13 @@
 package edu.vit.corejava.basics;
 
-/**
- * Description: Demo Java Program
+/*
  * 
- * @author: Anthoniraj Amalanathan
- * @since: 19-Jul-2022
+ * @author Anthoniraj Amalanathan
+ * @since 21-Jul-2022
  */
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Demo Java Program!");
+
     }
 }
