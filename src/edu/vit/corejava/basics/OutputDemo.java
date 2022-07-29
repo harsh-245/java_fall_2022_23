2a2bd78e464cfc981242b8a40fadb0f8efa28791
anthoniraj <anthoniraja@gmail.com>
2022-07-29 05:54:10 +0530

    updated output code

--- src/edu/vit/corejava/basics/OutputDemo.java
+++ src/edu/vit/corejava/basics/OutputDemo.java
@@ -13,10 +13,15 @@ public class OutputDemo {
     public final static Logger logger = Logger.getLogger("global");
 
     public static void main(String[] args) {
+        /* Method-1 using PrintWriter */
         PrintWriter out = new PrintWriter(System.out);
         out.println("Hi There!");
         out.flush();
+
+        /* Method-2 using System.out.print */
         System.out.println("Second Line");
+
+        /* Method-3 using Logger */
         logger.info("This is an information!");
         logger.warning("Be careful!");
         logger.severe("Error Happened!");
